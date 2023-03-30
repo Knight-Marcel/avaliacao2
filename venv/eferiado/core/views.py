14b84e9a3bf04a2b0c4ed1939d041622c5bcded8
Marcel de Menezes Araujo <araujomarcel89@gmail.com>
2023-03-30 17:44:40 -0300

    controle do dia do feriado

--- venv/eferiado/core/views.py
+++ venv/eferiado/core/views.py
@@ -1,7 +1,16 @@
 from django.shortcuts import render
+from datetime import date
 
-from django.http import HttpResponse
-def natal(request):
-    contexto = {'carnaval': True,
-        'natal': False}
-    return render(request, 'natal.html', contexto)
+
+
+def natal(resquest):
+    hoje = date.today()
+    natal = hoje.month == 12 and hoje.day == 25
+    tiradentes = hoje.month == 4 and hoje.day == 21
+    contexto = {'natal': natal, 'tiradentes': tiradentes}
+    return render(resquest, 'natal.html', contexto)
+    
+
+
+
+
--- venv/eferiado/eferiado/settings.py
+++ venv/eferiado/eferiado/settings.py
@@ -38,7 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'core',
-]
+    ]
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
